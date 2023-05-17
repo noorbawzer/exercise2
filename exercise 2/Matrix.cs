0890f23ff22a1ce75fb92bc1f372503e2a2d1294
noorbawzer <133597621+noorbawzer@users.noreply.github.com>
2023-05-18 01:41:41 +0300

    Add files via upload

---
+++ exercise 2/Matrix.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+    class Matrix
+    {
+
+        private int rows;
+        private int columns;
+        private int[,] matrix;
+        public Matrix(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            matrix = new int[rows, columns];
+        }
+        public void ReadMatrix()
+        {
+            Console.WriteLine("enter the elements of the matrix :");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write("[" + i + "]" + "[" + j + "] =");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+        public void Transpose()
+        {
+            int[,] TransposedMatrix = new int[columns, rows];
+            Console.WriteLine("Transpose is :");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                     TransposedMatrix[j,i]=matrix[i,j];
+                }
+            }
+            matrix = TransposedMatrix;
+            int temp = rows;
+            rows = columns;
+            columns = temp;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{ matrix[i, j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
+
+  
---
+++ exercise 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number of rows :");
+           int rows= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the number of columns : ");
+           int columns = Convert.ToInt32(Console.ReadLine());
+            Matrix mat = new Matrix(rows,columns);
+            mat.ReadMatrix();
+            Console.WriteLine(" the Matrix is :");
+            mat.Print();
+            mat.Transpose();
+            mat.Print();
+        }
+    }
+}
